c85c995b78633998c2da9502cb5d022cf8484378
Toby16 <bolajibaasit14@gmail.com>
2023-02-21 12:25:27 -0500

    insertion sort on doubly linked list

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,61 @@
+#include "sort.h"
+#include <stdlib.h>
+
+void insertion_sort_list(listint_t **list);
+
+/**
+ * insertion_sort_list - function to perform 'insertion sort'
+ *	on a list
+ * @list: argument of type 'listint_t **'
+ *	doubly linked list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *i, *j, *temp;
+	int key;
+
+	temp = malloc(sizeof(listint_t));
+
+	/* check if malloc encounters error */
+	if (temp != NULL)
+	{
+		temp = *list;
+		temp = temp->next;
+
+		for (i = temp; i != NULL; i = i->next)
+		{
+			key = i->n;
+			j = i->prev;
+
+			while ((j != NULL) && (j->n > key))
+			{
+				/* swap nodes */
+				i->prev = j->prev;
+				j->prev = i;
+				j->next = i->next;
+
+				if (i->next != NULL)
+				{
+					i->next->prev = j;
+				}
+
+				i->next = j;
+
+				if (i->prev == NULL)
+				{
+					*list = i;
+				}
+				else
+				{
+					i->prev->next = i;
+				}
+
+				j = i->prev;
+				print_list(*list);
+			}
+		}
+	}
+}
+
+
