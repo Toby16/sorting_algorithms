46a7cca5600cd9073428ebe078daa6abee50ba92
Toby16 <bolajibaasit14@gmail.com>
2023-02-21 12:25:05 -0500

    Header file | update

--- sort.h
+++ sort.h
@@ -25,5 +25,7 @@ void quick_sort_helper(int *array, int lo, int hi, size_t size);
 void quick_sort(int *array, size_t size);
 void swap(int *a, int *b);
 int partition(int *array, int lo, int hi, size_t size);
+void insertion_sort_list(listint_t **list);
+listint_t *create_listint(const int *array, size_t size);
 
 #endif
